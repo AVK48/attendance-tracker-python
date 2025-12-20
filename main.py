4ae0a811ee43a23c6f1d1dde6930db5fb47db7f2
AENNAM VINAY KUMAR <aennamvinaykumar@gmail.com>
2025-12-20 21:19:36 +0530

    Implement add_student function for attendance tracker
    Added functionality to add students to the attendance tracker.

--- main.py
+++ main.py
@@ -3,3 +3,46 @@ def main():
 
 if __name__ == "__main__":
     main()
+import csv
+
+DATA_FILE = "data/students.csv"
+
+def add_student():
+    roll_no = input("Enter roll number: ").strip()
+    name = input("Enter student name: ").strip()
+    if not roll_no or not name:
+        print("Invalid input")
+        return
+    with open(DATA_FILE, "r", newline="") as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            if row["roll_no"] == roll_no:
+                print("Roll number already exists")
+                return
+    with open(DATA_FILE, "a", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow([roll_no, name])
+    print("student added successfully")
+
+def main():
+    print("Attendance Tracker - Version 1")
+    while True:
+        print("\n1.Add student")
+        print(2.Exit")
+        choice = input("choose option:")
+
+        if choice == "1":
+            add_student()
+        elif choice == "2":
+            print("Exiting..")
+            break
+        else:
+            print("Invalid choice")
+
+if __name__ == "__main__":
+    main()
+            
+
+   
+
+  
